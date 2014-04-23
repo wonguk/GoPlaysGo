8649610175c9fbd4a29f53324609f895e9450c83
dbestCMU <dbest@andrew.cmu.edu>
2014-04-23 02:00:56 -0400

    Updated game logic

--- src/github.com/cmu440/goplaysgo/gogame/gamelogic.go
+++ src/github.com/cmu440/goplaysgo/gogame/gamelogic.go
@@ -14,7 +14,7 @@ type Board struct {
 
 func makeBoard(size int) Board {
 	Bd := new(Board)
-	Bd.Turn = 0
+	Bd.Turn = 1
 	Bd.Grid = make([][]Stones, size)
 	for index := range Bd.Grid {
 		Bd.Grid[index] = make([]Stones, size)
@@ -31,93 +31,160 @@ func isLegalMove(grid [][]Stones, player string, xpos int, ypos int) int {
 	return 1
 }
 
-func FindPosChain(chain []int, xpos int, ypos int) int {
+func FindPosChain(chain []int, ypos int, xpos int) int {
+	//fmt.Println("Called FindPosChain:", chain, xpos, ypos)
 	for index := 0; index < len(chain); index += 2 {
-		xindex := chain[index]
-		yindex := chain[index+1]
+		yindex := chain[index]
+		xindex := chain[index+1]
 		if (xpos == xindex) && (ypos == yindex) {
+			//fmt.Println("Found it")
 			return 0
 		}
 	}
+	//fmt.Println("Not here")
 	return 1
 }
 
-func FindStoneChain(grid [][]Stones, xpos int, ypos int, player string, chain []int, count int) ([]int, int) {
-	fmt.Println("Called FindStoneChain at:", xpos, " ", ypos)
+func FindStoneChain(grid [][]Stones, ypos int, xpos int, player string, chain []int, count int) ([]int, int) {
+	//fmt.Println("Called FindStoneChain at:", xpos, " ", ypos)
 	if xpos > 0 {
-		TempStoneLeft := grid[xpos-1][ypos]
-		fmt.Println("TempStoneLeft:", TempStoneLeft)
-		if (TempStoneLeft.Player == player) && (FindPosChain(chain, xpos, ypos) == 1) {
-			fmt.Println("Turned left")
-			chain[count] = xpos - 1
-			count++
+		TempStoneLeft := grid[ypos][xpos-1]
+		//fmt.Println("TempStoneLeft:", TempStoneLeft)
+		if (TempStoneLeft.Player == player) && (FindPosChain(chain, ypos, xpos) == 1) {
+			//fmt.Println("Turned left")
 			chain[count] = ypos
 			count++
-			chain, count = FindStoneChain(grid, xpos-1, ypos, player, chain, count)
+			chain[count] = xpos
+			count++
+			chain, count = FindStoneChain(grid, ypos, xpos-1, player, chain, count)
 		}
 	}
 	if len(grid)-1 > xpos {
-		TempStoneRight := grid[xpos+1][ypos]
-		fmt.Println("TempStoneRight:", TempStoneRight)
-		if (TempStoneRight.Player == player) &&(FindPosChain(chain, xpos, ypos) == 1) {
-			fmt.Println("Turned right")
-			chain[count] = xpos + 1
-			count++
+		TempStoneRight := grid[ypos][xpos+1]
+		//fmt.Println("TempStoneRight:", TempStoneRight)
+		if (TempStoneRight.Player == player) && (FindPosChain(chain, ypos, xpos) == 1) {
+			//fmt.Println("Turned right")
 			chain[count] = ypos
 			count++
-			chain, count = FindStoneChain(grid, xpos+1, ypos, player, chain, count)
+			chain[count] = xpos + 1
+			count++
+			chain, count = FindStoneChain(grid, ypos, xpos+1, player, chain, count)
 		}
 	}
 	if ypos > 0 {
-		TempStoneUp := grid[xpos][ypos-1]
-		fmt.Println("TempStoneUp:", TempStoneUp)
-		if (TempStoneUp.Player == player) && (FindPosChain(chain, xpos, ypos) == 1) {
-			fmt.Println("Turned Up")
-			chain[count] = xpos
-			count++
+		TempStoneUp := grid[ypos-1][xpos]
+		//fmt.Println("TempStoneUp:", TempStoneUp)
+		if (TempStoneUp.Player == player) && (FindPosChain(chain, ypos, xpos) == 1) {
+			//fmt.Println("Turned Up")
 			chain[count] = ypos - 1
 			count++
-			chain, count = FindStoneChain(grid, xpos, ypos-1, player, chain, count)
+			chain[count] = xpos
+			count++
+			chain, count = FindStoneChain(grid, ypos-1, xpos, player, chain, count)
 		}
 	}
 	if len(grid)-1 > ypos {
-		TempStoneDown := grid[xpos][ypos+1]
-		fmt.Println("TempStoneDown:", TempStoneDown)
-		if (TempStoneDown.Player == player) && (FindPosChain(chain, xpos, ypos) == 1) {
-			fmt.Println("Turned Down")
-			chain[count] = xpos
-			count++
+		TempStoneDown := grid[ypos+1][xpos]
+		//fmt.Println("TempStoneDown:", TempStoneDown)
+		if (TempStoneDown.Player == player) && (FindPosChain(chain, ypos, xpos) == 1) {
+			//fmt.Println("Turned Down")
 			chain[count] = ypos + 1
 			count++
-			chain, count = FindStoneChain(grid, xpos, ypos+1, player, chain, count)
+			chain[count] = xpos
+			count++
+			chain, count = FindStoneChain(grid, ypos+1, xpos, player, chain, count)
 		}
 	}
-	chain[count] = xpos
-	count++
-	chain[count] = ypos
-	count++
+	if FindPosChain(chain, ypos, xpos) == 1 {
+		chain[count] = ypos
+		count++
+		chain[count] = xpos
+		count++
+		return chain, count
+	}
 	return chain, count
 }
 
-/*
-func removePieces (grid [][]Stones,turn int) [][]Stones {
-	RemoveList := make([]int,turn)
-	RemoveCount := 0
-	for yindex := 0; yindex < len(grid); yindex++ {
-	    for xindex :=0; xindex < len(grid); xindex++ {
-	        TestStone := grid[yindex][xindex]
-		if (TestStone.Player != "")
+func CountTerritory(grid [][]Stones, pieces []int, count int) int {
+	TCount := 0
+	//fmt.Println("Called Count Territory with:", pieces)
+	for index := 0; index < count; index = index + 2 {
+		ypos := pieces[index]
+		xpos := pieces[index+1]
+		if xpos > 0 {
+			//fmt.Println("Here with:", ypos, xpos)
+			TempStone := grid[ypos][xpos-1]
+			if TempStone.Player == "" {
+				TempStone.Player = "Taken"
+				TCount++
+				grid[ypos][xpos-1] = TempStone
+			}
+		}
+		if xpos < len(grid)-1 {
+			TempStone := grid[ypos][xpos+1]
+			if TempStone.Player == "" {
+				TempStone.Player = "Taken"
+				TCount++
+				grid[ypos][xpos+1] = TempStone
+			}
+		}
+		if ypos > 0 {
+			TempStone := grid[ypos-1][xpos]
+			if TempStone.Player == "" {
+				TempStone.Player = "Taken"
+				TCount++
+				grid[ypos-1][xpos] = TempStone
+			}
+		}
+		if ypos < len(grid)-1 {
+			TempStone := grid[ypos+1][xpos]
+			if TempStone.Player == "" {
+				TempStone.Player = "Taken"
+				TCount++
+				grid[ypos+1][xpos] = TempStone
+			}
+		}
+	}
+	for yindex := 0; yindex < len(grid); yindex = yindex + 1 {
+		for xindex := 0; xindex < len(grid); xindex = xindex + 1 {
+			TempStone := grid[yindex][xindex]
+			if TempStone.Player == "Taken" {
+				TempStone.Player = ""
+				grid[yindex][xindex] = TempStone
+			}
+		}
+	}
+	return TCount
+}
 
-
-*/
-
-func makeMove(grid [][]Stones, player string, xpos int, ypos int, turn int) [][]Stones {
+func makeMove(grid [][]Stones, player string, ypos int, xpos int, turn int) ([][]Stones, int) {
 	PlacedStone := new(Stones)
 	PlacedStone.Player = player
 	PlacedStone.Turn = turn
-	if isLegalMove(grid, player, xpos, ypos) == 1 {
+	if isLegalMove(grid, player, ypos, xpos) == 1 {
 		grid[ypos][xpos] = *PlacedStone
-		return grid
+		for yindex := 0; yindex < len(grid); yindex++ {
+			for xindex := 0; xindex < len(grid); xindex++ {
+				if grid[ypos][xpos].Player != "" {
+					//fmt.Println(grid)
+					EmptyChain := make([]int, len(grid)*len(grid))
+					StoneChain, ChainLen := FindStoneChain(grid, yindex, xindex, player, EmptyChain, 0)
+					//fmt.Println(StoneChain, ChainLen)
+					TerrorityCount := CountTerritory(grid, StoneChain, ChainLen)
+					if TerrorityCount == 0 {
+						//fmt.Println("Removing pieces")
+						for index := 0; index < ChainLen*2; index = index + 2 {
+							xremove := StoneChain[index]
+							yremove := StoneChain[index]
+							grid[yremove][xremove].Player = ""
+							grid[yremove][xremove].Turn = 0
+						}
+					}
+				}
+			}
+		}
+
+		return grid, turn + 1
 	}
-	return grid //Got an invalid move
+	return grid, turn + 1 //Got an invalid move
 }
