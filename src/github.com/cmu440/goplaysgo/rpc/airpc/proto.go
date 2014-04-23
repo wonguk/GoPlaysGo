dc6a2157363f8673e5c29fa4b5be9a42f95b75d9
Won Gu Kang <kangwo1@gmail.com>
2014-04-23 07:36:01 -0400

    Made modifications to the airpc

--- src/github.com/cmu440/goplaysgo/rpc/airpc/proto.go
+++ src/github.com/cmu440/goplaysgo/rpc/airpc/proto.go
@@ -1,6 +1,9 @@
 package airpc
 
-import "github.com/cmu440/goplaysgo/rpc/mainrpc"
+import (
+	"github.com/cmu440/goplaysgo/gogame"
+	"github.com/cmu440/goplaysgo/rpc/mainrpc"
+)
 
 type Status int
 
@@ -16,14 +19,12 @@ const (
 // so we only need to send the moves we make back and forth
 type NextMoveArgs struct {
 	Player string
-	XPos   int
-	YPos   int
+	Move   gogame.Move
 }
 
 type NextMoveReply struct {
 	Status Status
-	XPos   int
-	YPos   int
+	Move   gogame.Move
 }
 
 type CheckArgs struct {
@@ -36,15 +37,18 @@ type CheckReply struct {
 
 // I think we may need another RPC call before InitGame for the 2PC
 type InitGameArgs struct {
-	size int
+	Player   string
+	Hostport string
+
+	Size int
 }
 
 type IntGameReply struct {
 	Status Status
 }
 
-//No args needed
 type StartGameArgs struct {
+	Player string
 }
 
 //StartGame should reply with the
