c145bafb58991db67c0a1f9a7de3faca63c118c8
Won Gu Kang <kangwo1@gmail.com>
2014-04-26 15:51:40 -0400

    Fixed indexing problem in ai implementations

--- src/github.com/cmu440/goplaysgo/ai/ai_impl.go
+++ src/github.com/cmu440/goplaysgo/ai/ai_impl.go
@@ -11,7 +11,7 @@ func NextMove(board gogame.Board, player gogame.Player) gogame.Move {
 	var x int = rand.Intn(len(board.Grid))
 	var y int = rand.Intn(len(board.Grid))
 
-	for board.Grid[x][y].Player != "" {
+	for board.Grid[y][x].Player != "" {
 		x = rand.Intn(len(board.Grid))
 		y = rand.Intn(len(board.Grid))
 	}
